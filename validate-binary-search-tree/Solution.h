66c1cb4f3225b92c8ed003b690f07b82681562ab
Lu Gao <lu.gao@getcruise.com>
2020-04-11 22:29:52 -0700

    remve unnecessary folders and file

--- validate-binary-search-tree/Solution.h
+++ validate-binary-search-tree/Solution.h
@@ -42,7 +42,17 @@ This is a binary search tree.
  * }
  */
 
-class Solution {
+/*
+    summary:
+    At each node, we create the interval for all the points of the left subtree and right subtree
+    - The interval of the left subtree is (-inf, currentNode->val)
+    - The interval of the right subtree is (currentNode->val, +inf)
+    The trick is how to define +/-inf? 
+    - Solution1A: if the type of val is int, we can use LONG_MIN and LONG_MAX to represent -inf and +inf, respectively
+    - Solution1B: it is possible that the type of val is long long, then we will use nullptr to represent inf
+*/
+
+class Solution1A {
 public:
     /**
      * @param root: The root of binary tree.
@@ -61,6 +71,34 @@ private:
         bool condition1 = currentNode->val > lowerLimit && currentNode->val < upperLimit;
         bool condition2 = leftTrace && rightTrace; 
         return condition1 && condition2; 
-    } 
-    
+    }     
+};
+
+class Solution1B {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    bool isValidBST(TreeNode * root) {
+        return _isValidBST(root, NULL, NULL); 
+    }
+private: 
+    bool _isValidBST(TreeNode* currentNode, int* lowerLimit, int* upperLimit) {
+        if (currentNode == NULL) {
+            return true; 
+        }
+        bool leftTrace = _isValidBST(currentNode->left, lowerLimit, &currentNode->val);
+        bool rightTrace = _isValidBST(currentNode->right, currentNode->val, &upperLimit);
+        /*
+            if a limit is not NULL, the limit is set from the parent node. 
+            we then compare the current node value with the limit            
+        */ 
+        bool condition1A = (lowerLimit != NULL && currentNode->val < *lowerLimit);
+        bool condition1B = (upperLimit != NULL && currentNode->val > *upperLimit);
+        if(condition1A || condition1B) {
+            return false; 
+        }                
+        return leftTrace && rightTrace;  
+    }     
 };
